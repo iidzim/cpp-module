eaddc0a8994ca2f3ee05e7f4ef45796e15484ca8
ikram idzim <mac@MacBook-Pro-de-ikram.local>
2021-09-19 23:12:04 +0100

    .

--- module00/ex01/phonebook.cpp
+++ module00/ex01/phonebook.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 17:36:50 by iidzim            #+#    #+#             */
-/*   Updated: 2021/09/18 17:38:45 by iidzim           ###   ########.fr       */
+/*   Updated: 2021/09/19 23:11:23 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,32 @@
 
 int main(void)
 {
-    
+    phonebook my_pb;
+    std::string action;
+    int n;
+
+    n = 0;
+    while (1)
+    {
+        std::cout << "ADD to add a contact" << std::endl;
+        std::cout << "SEARCH to display the phonebook" << std::endl;
+        std::cout << "EXIT to exit the prg" << std::endl;
+        std::cin >> action;
+        if (action.compare("add") == 0)
+        {
+            
+        }
+        else if (action.compare("search") == 0)
+        {
+
+        }
+        else if (action.compare("exit") == 0)
+        {
+            std::cout << "bye" << std::endl;
+            break;
+        }
+        else
+            std::cout << "wrong operation" << std::endl;
+    }
     return (0);
 }
