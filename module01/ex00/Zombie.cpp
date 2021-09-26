59e9936e4d264555943b24e621fe10075f4931ab
Ikram Idzim <iidzim@e0r4p2.1337.ma>
2021-09-26 14:20:14 +0100

    01-00

---
+++ module01/ex00/Zombie.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/26 13:20:30 by iidzim            #+#    #+#             */
+/*   Updated: 2021/09/26 14:19:28 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(){
+    std::cout << "constructor" << std::endl;
+}
+
+Zombie::~Zombie(){
+    std::cout << "detructor" << std::endl;
+}
+
+void Zombie::set_name(std::string name){
+    this->_name = name;
+}
+
+std::string Zombie::get_name(void){
+    return (this->_name);
+}
+
+void Zombie::announce(void){
+    
+}
---
+++ module01/ex00/Zombie.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/26 13:11:27 by iidzim            #+#    #+#             */
+/*   Updated: 2021/09/26 14:11:17 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie{
+private:
+    std::string _name;
+public:
+    Zombie();
+    ~Zombie();
+    void announce(void);
+    void set_name(std::string name);
+    std::string get_name(void);
+};
+
+#endif
