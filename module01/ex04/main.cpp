67c5b1d792a95c6dbc5f288fb1365d130f0ba260
Ikram Idzim <iidzim@e0r4p1.1337.ma>
2021-09-29 17:49:41 +0100

    01-04

---
+++ module01/ex04/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 17:45:39 by iidzim            #+#    #+#             */
+/*   Updated: 2021/09/29 17:49:20 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main (void){
+
+    std::string s1, s2, filename;
+    //?open the file, and write its contents to FILENAME.replace
+    //* replacing every occurrence of s1 with s2
+    return (0);
+}
