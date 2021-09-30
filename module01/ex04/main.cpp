1590cae17e2c9f3d6a217bfe61e46563da0f4430
Ikram Idzim <iidzim@e0r4p1.1337.ma>
2021-09-30 13:24:52 +0100

    01-05

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,17 +6,43 @@
 /*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 17:45:39 by iidzim            #+#    #+#             */
-/*   Updated: 2021/09/29 17:49:20 by iidzim           ###   ########.fr       */
+/*   Updated: 2021/09/30 13:21:29 by iidzim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 int main (void){
 
-    std::string s1, s2, filename;
-    //?open the file, and write its contents to FILENAME.replace
-    //* replacing every occurrence of s1 with s2
+    std::ifstream ifs("test");
+    std::ofstream ofs("test.replace");
+    if (!ifs.good() || !ofs.good())
+    {
+        std::cout << "Invalid stream " << std::endl;
+        return (EXIT_FAILURE);
+    }
+    std::string buff;
+    std::string s1, s2;
+    size_t pos;
+    s1 = "philosophy";
+    s2 = "CPP";
+    while (!ifs.eof()){
+        getline(ifs, buff);
+        pos = buff.find(s1);
+        while (pos < buff.size()){
+            std::string str1 = buff.substr(0, pos);
+            std::string str2 = buff.substr(pos + s1.size());
+            buff = str1 + s2 + str2;
+            size_t i = buff.find(s1);
+            if (pos == i)
+                break;
+            pos = buff.find(s1);
+        }
+        ofs << buff << std::endl;
+    }
+    ifs.close();
+    ofs.close();
     return (0);
 }
