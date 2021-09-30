5e0c741a59daf0e721150b8fffc6fe009c85283a
Ikram Idzim <iidzim@e0r4p1.1337.ma>
2021-09-30 18:12:13 +0100

    karen

--- module01/ex03/HumanB.cpp
+++ module01/ex03/HumanB.cpp
@@ -6,16 +6,15 @@
 /*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 15:49:48 by iidzim            #+#    #+#             */
-/*   Updated: 2021/09/29 17:38:17 by iidzim           ###   ########.fr       */
+/*   Updated: 2021/09/30 18:08:06 by iidzim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name){// : _name(name)
-
-    this->_name = name;
-    // this->_weapon = 
+HumanB::HumanB(std::string name) : _name(name)
+{
+    return ;
 }
 
 HumanB::~HumanB(void){
