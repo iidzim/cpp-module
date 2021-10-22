af26c53b5209c2645fd31606ec99ac092d071fcb
Ikram Idzim <iidzim@e2r5p12.1337.ma>
2021-10-22 13:47:47 +0100

    08-02

---
+++ module08/ex02/mutantstack.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iidzim <iidzim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 13:44:35 by iidzim            #+#    #+#             */
+/*   Updated: 2021/10/22 13:47:32 by iidzim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iostream>
+#include <string>
+#include <exception>
+
+class mutantstack{
+  private:
+    
+  public:
+    
+};
+
+#endif
